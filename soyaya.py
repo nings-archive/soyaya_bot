011e5940e702ff13ce652906f5d6ce29563cc1e6
ning <ningyuan.sg@gmail.com>
2017-11-18 11:06:05 +0800

    Make log a RotatingFileHandler
    Change log file to rotate with logging module's RotatingFileHandler. Yet to
    figure out how to mount this ./volume to container's /soyaya/volume in the
    Dockerfile

--- soyaya.py
+++ soyaya.py
@@ -1,12 +1,15 @@
 #! /usr/bin/env python3
-import datetime, logging
+import logging, logging.handlers
 from telegram.ext import Updater, CommandHandler
 import commands
 from keys import api_key
 
+rotating_handler = logging.handlers.RotatingFileHandler(
+    'volume/soyaya.log', maxBytes=10000, backupCount=5, encoding='utf-8'
+)
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
-    level=logging.INFO
+    level=logging.INFO, handlers=[rotating_handler]
 )
 logging.info('Program start')
 
